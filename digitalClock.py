0b15b32c06af724ae2e91acb1284e61f17d4460f
parrenteks <ross@parrenteks.com>
2023-12-31 11:05:25 -0700

    added welcome message

--- digitalClock.py
+++ digitalClock.py
@@ -1,9 +1,14 @@
-# reference techTFQ for Git tutorial
-# https://www.youtube.com/watch?v=kY5HtrkjSj0 , 17:43
+# try again, from techTFQ tutorial about Git
+# from YouTube https://www.youtube.com/watch?v=kY5HtrkjSj0
+
 from tkinter import Tk, Label
 
 window = Tk()
-window.title("Digital Clock")
 window.geometry("600x300")
+window.title("Digital Clock")
+window.config(bg="steelblue")
 
+label = Label(window, text="Welcome", font=("Arial Black",78,"bold"), bg="steelblue",fg="white")
+label.pack(pady=100)
+#testing
 window.mainloop()
