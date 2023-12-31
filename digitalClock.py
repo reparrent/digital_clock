683134c4e9efda9d0073e074a352e4e6e38b5077
parrenteks <ross@parrenteks.com>
2023-12-31 08:22:45 -0700

    added digital clock program displaying window only using ktinker

---
+++ digitalClock.py
@@ -0,0 +1,9 @@
+# reference techTFQ for Git tutorial
+# https://www.youtube.com/watch?v=kY5HtrkjSj0 , 17:43
+from tkinter import Tk, Label
+
+window = Tk()
+window.title("Digital Clock")
+window.geometry("600x300")
+
+window.mainloop()
