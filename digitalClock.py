5944b9101ea251608d3fffb5aebd478746d38393
parrenteks <ross@parrenteks.com>
2023-12-31 11:25:15 -0700

    added clock display

--- digitalClock.py
+++ digitalClock.py
@@ -1,6 +1,6 @@
 # try again, from techTFQ tutorial about Git
 # from YouTube https://www.youtube.com/watch?v=kY5HtrkjSj0
-
+from datetime import datetime
 from tkinter import Tk, Label
 
 window = Tk()
@@ -11,4 +11,11 @@ window.config(bg="steelblue")
 label = Label(window, text="", font=("Arial Black",78,"bold"), bg="steelblue",fg="white")
 label.pack(pady=100)
 
+def clock():
+    time = datetime.now().strftime("%H:%M:%S")
+
+    label.configure(text=time)
+    label.after(500, clock)
+
+clock()
 window.mainloop()
